cc2d2d4fe239f7a7b4ecbe18ddb9895db80dd890
Glaucos Cavalcante <101951153+BigGlaucos@users.noreply.github.com>
2023-06-05 12:12:24 -0300

    Add files via upload

---
+++ js/notification.js
@@ -0,0 +1,34 @@
+const notificacoesEscolares = {
+  notificacao1: {
+    titulo: 'Reunião de Pais e Professores',
+    informacao: 'Local: Auditório da escola.',
+  },
+  notificacao2: {
+    titulo: 'Prova de Matemática',
+    informacao: 'Conteúdo: Equações e Geometria.',
+  },
+  notificacao3: {
+    titulo: 'Excursão ao Museu',
+    informacao: 'Saída da escola às 9h.',
+  },
+  notificacao4: {
+    titulo: 'Aula de Educação Física Cancelada',
+    informacao: 'A aula de Educação Física foi cancelada.',
+  },
+  notificacao5: {
+    titulo: 'Trabalho de História',
+    informacao: 'Tema: Revolução Francesa.',
+  },
+  notificacao6: {
+    titulo: 'Recesso Escolar',
+    informacao: 'Não haverá aulas.',
+  },
+  notificacao7: {
+    titulo: 'Visita ao Planetário',
+    informacao: 'Local: Planetário Municipal.',
+  },
+  notificacao8: {
+    titulo: 'Palestra sobre Meio Ambiente',
+    informacao: 'Palestrante: Biólogo renomado.',
+  },
+};
